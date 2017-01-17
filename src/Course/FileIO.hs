a5808227ab8a2e1e150c9b3e2448b822b7866815
wangwangwar <wangwangwar@gmail.com>
2017-01-17 12:27:42 +0800

    FileIO

--- src/Course/FileIO.hs
+++ src/Course/FileIO.hs
@@ -58,7 +58,7 @@ To test this module, load ghci in the root of the project directory, and do
 Example output:
 
 $ ghci
-GHCi, version ... 
+GHCi, version ...
 Loading package...
 Loading ...
 [ 1 of 28] Compiling (etc...
@@ -74,13 +74,20 @@ the contents of b
 ============ share/c.txt
 the contents of c
 
+
+  getArgs :: IO (List Chars)
+  putStrLn :: Chars -> IO ()
+  readFile :: Chars -> IO Chars
+  lines :: Chars -> List Chars
+  void :: IO a -> IO ()
 -}
 
 -- /Tip:/ use @getArgs@ and @run@
 main ::
   IO ()
-main =
-  error "todo: Course.FileIO#main"
+main = do
+  args <- getArgs
+  foldRight (>>) (return ()) $ map run args
 
 type FilePath =
   Chars
@@ -90,32 +97,28 @@ type FilePath =
 run ::
   FilePath
   -> IO ()
-run =
-  error "todo: Course.FileIO#run"
+run filename = getFile filename >>= \(_, content) -> getFiles (lines content) >>= (getFiles . map fst) >>= printFiles
 
 -- Given a list of file names, return list of (file name and file contents).
 -- Use @getFile@.
 getFiles ::
   List FilePath
   -> IO (List (FilePath, Chars))
-getFiles =
-  error "todo: Course.FileIO#getFiles"
+getFiles list = sequence $ map getFile list
 
 -- Given a file name, return (file name and file contents).
 -- Use @readFile@.
 getFile ::
   FilePath
   -> IO (FilePath, Chars)
-getFile =
-  error "todo: Course.FileIO#getFile"
+getFile path = readFile path >>= \content -> return (path, content)
 
 -- Given a list of (file name and file contents), print each.
 -- Use @printFile@.
 printFiles ::
   List (FilePath, Chars)
   -> IO ()
-printFiles =
-  error "todo: Course.FileIO#printFiles"
+printFiles list = foldRight (>>) (return ()) $ map (uncurry printFile) list
 
 -- Given the file name, and file contents, print them.
 -- Use @putStrLn@.
@@ -123,5 +126,4 @@ printFile ::
   FilePath
   -> Chars
   -> IO ()
-printFile =
-  error "todo: Course.FileIO#printFile"
+printFile path content = putStrLn ("============ " ++ path) >> putStrLn content
